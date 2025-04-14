91e3972602cf0bda6de14cc1d6be3ec3e1047ebf
Manulik <125672053+Otocolobus-manules@users.noreply.github.com>
2025-04-14 12:16:38 +0300

    Ангуляй

---
+++ src/app/hero.ts
@@ -0,0 +1,16 @@
+export interface Hero {
+  id: number;
+  name: string;
+  protection_from_holy_damage: number;
+  protection_from_darkness: number;
+  the_level_of_insight: string;
+  power: string;
+  isLegendary: boolean;
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
